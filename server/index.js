622822c346fcc7b9cbd4f09bbb14fd74a02b4a19
Juan Ruiz <jryanruiz@live.com>
2022-09-21 15:08:05 -0700

    A

--- server/index.js
+++ server/index.js
@@ -9,18 +9,22 @@ const app = express()
 app.use(express.json())
 
 
-// app.use('/', express.static(path.join(dirname, '../client/index.html')))
+app.use('/', express.static(path.join(dirname, '../client/index.html')))
 
-// app.use(express.static(path.join(dirname, '../client')))
+app.use(express.static(path.join(dirname, '../client')))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'))
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/index.html'))
+// })
 
 
-app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/main.js'))
-})
+// app.get('/js', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/main.js'))
+// })
+
+// app.get('/css', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/style.css'))
+// })
 app.listen(port, () => {
     console.log('Docked at port ' + port)
 })
